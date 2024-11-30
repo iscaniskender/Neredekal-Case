79d1787536c712b51dcc4368d55543469fbe6cd9
İskender İşcan <iscaniskender@gmail.com>
2024-11-30 19:39:17 +0300

    gitignore clear

---
+++ ReportService.Api/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReportService.Api.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
